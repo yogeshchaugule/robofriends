451be904f116d6688534da1d7d79e37ce610aea5
Yogesh Chaugule <yogesh.chaugule@loylty.com>
2022-08-18 18:08:15 +0530

    Reverted react dom changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
@@ -7,7 +7,15 @@ import 'tachyons';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <Hello greeting={'Hello World'}  />
+    <HelloWorld greetings={'Hello World'} /> */}
+    <App />
+    {/* <CardList /> */}
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
